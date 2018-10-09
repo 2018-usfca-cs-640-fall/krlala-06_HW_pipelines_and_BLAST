c53f2f6f212e57bd9432efdd4d8be6a5ee6d98e3
krlala <krlala@usfca.edu>
2018-10-09 00:34:20 +0000

    Initial commit with no code

---
+++ process_fierer_data.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Script to process Fierer 16S dataset
+# Including downloading from NCBI SRA
+# Running fastqc, trimming, converting
+# to fasta from fastq, and finally
+# BLASTING with command -line blastn
+
+# Karan Lala
+# krlala@usfca.edu
+# October , 2018
