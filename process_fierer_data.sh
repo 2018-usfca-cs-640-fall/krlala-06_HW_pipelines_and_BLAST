3a85406402d643a5d0ccee050b65feeae33a2826
krlala <krlala@usfca.edu>
2018-10-09 01:34:00 +0000

    Progress made in class

--- process_fierer_data.sh
+++ process_fierer_data.sh
@@ -9,3 +9,21 @@
 # Karan Lala
 # krlala@usfca.edu
 # October , 2018
+
+# the pipe and tail -n +2 is a handy way to exclude the first line
+for SRA_number in $(cut -f 6 data/metadata/fierer_forensic_hand_mouse_SraRunTable.txt | tail -n +2)
+do
+    fastq-dump -v $SRA_number -O data/raw_data
+done
+
+
+# Run fastqc on all the files
+fastqc data/raw_data/*.fastq --outdir=output/fastqc
+
+# Info on the Trimmomatic tool available here: http://www.usadellab.org/cms/index.php?page=trimmomatic
+TrimmomaticSE -threads 2 -phred33 data/raw_data/ERR1942280.fastq data/trimmed/$(basename -s .fastq ERR1942280.fastq).trim.fastq LEADING:5 TRAILING:5 SLIDINGWINDOW:8:25 MINLEN:150
+#Convert fastq file to fasta
+
+bioawk -c fastx '{print ">"$name"\n"$seq}' data/trimmed/filename.trim.fastq
+
+
