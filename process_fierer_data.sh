a63e05e3d379270e9ed01fdeb650e524cb4646c1
krlala <krlala@usfca.edu>
2018-10-13 10:33:02 +0000

    Added fastq to fasta bioawk

--- process_fierer_data.sh
+++ process_fierer_data.sh
@@ -11,19 +11,25 @@
 # October , 2018
 
 # the pipe and tail -n +2 is a handy way to exclude the first line
-for SRA_number in $(cut -f 6 data/metadata/fierer_forensic_hand_mouse_SraRunTable.txt | tail -n +2)
-do
-    fastq-dump -v $SRA_number -O data/raw_data
-done
+#for SRA_number in $(cut -f 6 data/metadata/fierer_forensic_hand_mouse_SraRunTable.txt | tail -n +2)
+#do
+#    fastq-dump -v $SRA_number -O data/raw_data
+#done
 
 
 # Run fastqc on all the files
-fastqc data/raw_data/*.fastq --outdir=output/fastqc
+#fastqc data/raw_data/*.fastq --outdir=output/fastqc
 
 # Info on the Trimmomatic tool available here: http://www.usadellab.org/cms/index.php?page=trimmomatic
-TrimmomaticSE -threads 2 -phred33 data/raw_data/ERR1942280.fastq data/trimmed/$(basename -s .fastq ERR1942280.fastq).trim.fastq LEADING:5 TRAILING:5 SLIDINGWINDOW:8:25 MINLEN:150
-#Convert fastq file to fasta
+for file in data/raw_data/*.fastq
+do
+  TrimmomaticSE -threads 2 -phred33 "$file" data/trimmed/$(basename -s .fastq "$file").trim.fastq LEADING:5 TRAILING:5 SLIDINGWINDOW:8:25 MINLEN:150
+done
 
-bioawk -c fastx '{print ">"$name"\n"$seq}' data/trimmed/filename.trim.fastq
+#Convert fastq file to fasta
+for file in data/trimmed/*
+do
+bioawk -c fastx '{print ">"$name"\n"$seq}' "$file" > data/trimmedfasta/$(basename -s .fastq "$file").trim.fasta
+done
 
 
