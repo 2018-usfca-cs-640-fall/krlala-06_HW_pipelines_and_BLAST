335f671f5f6de6c4a2537c97e19daec5d330b47a
krlala <krlala@usfca.edu>
2018-10-15 02:21:04 +0000

    Script complete, generates CSV files and stores them in output/Blast_results

--- process_fierer_data.sh
+++ process_fierer_data.sh
@@ -11,14 +11,14 @@
 # October , 2018
 
 # the pipe and tail -n +2 is a handy way to exclude the first line
-#for SRA_number in $(cut -f 6 data/metadata/fierer_forensic_hand_mouse_SraRunTable.txt | tail -n +2)
-#do
-#    fastq-dump -v $SRA_number -O data/raw_data
-#done
+for SRA_number in $(cut -f 6 data/metadata/fierer_forensic_hand_mouse_SraRunTable.txt | tail -n +2)
+do
+    fastq-dump -v $SRA_number -O data/raw_data
+done
 
 
 # Run fastqc on all the files
-#fastqc data/raw_data/*.fastq --outdir=output/fastqc
+fastqc data/raw_data/*.fastq --outdir=output/fastqc
 
 # Info on the Trimmomatic tool available here: http://www.usadellab.org/cms/index.php?page=trimmomatic
 for file in data/raw_data/*.fastq
@@ -26,10 +26,13 @@ do
   TrimmomaticSE -threads 2 -phred33 "$file" data/trimmed/$(basename -s .fastq "$file").trim.fastq LEADING:5 TRAILING:5 SLIDINGWINDOW:8:25 MINLEN:150
 done
 
-#Convert fastq file to fasta
+#Convert fastq file to fasta 
 for file in data/trimmed/*
 do
-bioawk -c fastx '{print ">"$name"\n"$seq}' "$file" > data/trimmedfasta/$(basename -s .fastq "$file").trim.fasta
+ bioawk -c fastx '{print ">"$name"\n"$seq}' "$file" > data/trimmedfasta/$(basename -s .fastq "$file").trim.fasta
 done
-
-
+#Adding code to use blastn to search for the top match of each sequence against the nt database
+for file in data/trimmedfasta/*
+do
+blastn -db /blast-db/nt -num_threads 2 -outfmt '10 sscinames std' -out output/Blast_results/$(basename -s .trim.fasta "$file").blastresults.csv -max_target_seqs 1 -negative_gilist /blast-db/2018-09-19_environmental_sequence.gi -query "$file"
+done
