393b20595c500750474787c734e36e6afd8ac75c
krlala <krlala@usfca.edu>
2018-10-17 01:55:36 +0000

    made shellcheck changes and checked if script works

--- process_fierer_data.sh
+++ process_fierer_data.sh
@@ -10,29 +10,32 @@
 # krlala@usfca.edu
 # October , 2018
 
-# the pipe and tail -n +2 is a handy way to exclude the first line
+# the pipe and tail -n +2 is a handy way
+# to exclude the first line
 for SRA_number in $(cut -f 6 data/metadata/fierer_forensic_hand_mouse_SraRunTable.txt | tail -n +2)
 do
-    fastq-dump -v $SRA_number -O data/raw_data
+    fastq-dump -v "$SRA_number" -O data/raw_data
 done
 
 
 # Run fastqc on all the files
 fastqc data/raw_data/*.fastq --outdir=output/fastqc
 
-# Info on the Trimmomatic tool available here: http://www.usadellab.org/cms/index.php?page=trimmomatic
+# Info on the Trimmomatic tool available here:
+# http://www.usadellab.org/cms/index.php?page=trimmomatic
 for file in data/raw_data/*.fastq
 do
-  TrimmomaticSE -threads 2 -phred33 "$file" data/trimmed/$(basename -s .fastq "$file").trim.fastq LEADING:5 TRAILING:5 SLIDINGWINDOW:8:25 MINLEN:150
+  TrimmomaticSE -threads 2 -phred33 "$file" data/trimmed/"$(basename -s .fastq "$file").trim.fastq" LEADING:5 TRAILING:5 SLIDINGWINDOW:8:25 MINLEN:150
 done
 
 #Convert fastq file to fasta 
 for file in data/trimmed/*
 do
- bioawk -c fastx '{print ">"$name"\n"$seq}' "$file" > data/trimmedfasta/$(basename -s .fastq "$file").trim.fasta
+ bioawk -c fastx '{print ">"$name"\n"$seq}' "$file" > data/trimmedfasta/"$(basename -s .fastq "$file").trim.fasta"
 done
-#Adding code to use blastn to search for the top match of each sequence against the nt database
+#Adding code to use blastn to search for the
+# top match of each sequence against the nt database
 for file in data/trimmedfasta/*
 do
-blastn -db /blast-db/nt -num_threads 2 -outfmt '10 sscinames std' -out output/Blast_results/$(basename -s .trim.fasta "$file").blastresults.csv -max_target_seqs 1 -negative_gilist /blast-db/2018-09-19_environmental_sequence.gi -query "$file"
+blastn -db /blast-db/nt -num_threads 2 -outfmt '10 sscinames std' -out output/Blast_results/"$(basename -s .trim.fasta "$file").blastresults.csv" -max_target_seqs 1 -negative_gilist /blast-db/2018-09-19_environmental_sequence.gi -query "$file"
 done
